2ba8513f5930dfb0677816c2693b58eeb1f0079d
Edwin <kastille84@gmail.com>
2021-03-26 15:39:54 -0400

    added redux-persist to our application

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,25 @@
 import { combineReducers } from 'redux';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //to user localstorage as persisted storage
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+
+// persist config
+const persistConfig = {
+  key: 'root', // at what point in our reducer do we want to start persisting
+  storage: storage, // storage type
+  // array containing strings name of reducers we want to persist
+  // 'cart', for cartReducer
+  // user is being persisted by firebase so we don't include it
+  whitelist: ['cart'] 
+}
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
 
-export default rootReducer;
+// exporting a modified version of our root reducer
+// but now with persistence capabilities
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from "redux-logger";
 
 import RootReducer from './root-reducer';
 
 const middlewares = [logger]
 
-const store = createStore(RootReducer, applyMiddleware(...middlewares));
+export const store = createStore(RootReducer, applyMiddleware(...middlewares));
+// this is a persisted version of our store
+export const persistor = persistStore(store);
 
-export default store;
+export default {store, persistor};
